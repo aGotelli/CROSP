b99ae1fa854d721b91ac2afa7b9b4938965d47ac
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-13 14:06:41 +0200

    testing

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,37 +1,13 @@
 
-//#include "cosserat_rod.hpp"
-
-#include <memory>
-
-#include "OSNI/include/ode_solver_interface.hpp"
-#include "OSNI/include/ode_a.hpp"
-#include "OSNI/include/ode_ab.hpp"
-#include "OSNI/include/ode_b.hpp"
-
-struct ODE : public OSNI::ODEA {
+#include "cosserat_rod.hpp"
 
 
-    ODE() : OSNI::ODEA(4) {}
-
-    virtual Eigen::MatrixXd computeMatrixAtChebyshevPoint(const unsigned int t_point)override
-    {
-        return Eigen::MatrixXd::Zero(4, 4);
-
-    }
-};
 
 
 int main(int argc, char *argv[])
 {
 
-    //QuaternionIntegrator q_int;
-
-
-    std::shared_ptr<OSNI::ODESolverInterface> ode_ptr;// = std::make_shared<OSNI::ODEA>( 4 );
-
-    ODE ode;
-
-
+    CROSP::CosseratRod cosserat_rod;
 
     return 0;
 }
