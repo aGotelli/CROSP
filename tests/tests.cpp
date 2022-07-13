9e43f07223b8c765bf0ddd8121c038d9b14abc61
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-13 15:38:48 +0200

    ok position and quaternions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,5 +9,29 @@ int main(int argc, char *argv[])
 
     CROSP::CosseratRod cosserat_rod;
 
+    constexpr unsigned int ne = 3;
+    constexpr unsigned int na = 3;
+    Eigen::Matrix<double, ne*na, 1> qe;
+    //  Here we give some value for the strain
+    qe <<   0,
+            0,
+            0,
+            1.2877691307032,
+           -1.63807499160786,
+            0.437406679142598,
+            0,
+            0,
+            0;
+
+    Eigen::Matrix<double, ne*na, 1> dot_qe, ddot_qe;
+    dot_qe.setZero();
+    ddot_qe.setZero();
+
+    cosserat_rod.updateParameterisation(qe, dot_qe, ddot_qe);
+
+    cosserat_rod.forwardKinematics();
+
+
+
     return 0;
 }
