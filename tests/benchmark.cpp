b088e23c53b7b86a8def7d1d8c5771f86505e923
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-18 18:00:50 +0200

    update

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -5,78 +5,58 @@
 
 
 
-
-
-
 int main(int argc, char *argv[])
 {
 
 
-//    benchmark::RegisterBenchmark("Forward Kinematics 5pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 5 );
+    benchmark::RegisterBenchmark("Forward Kinematics 10", [](benchmark::State &t_state){
 
-
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
-
-//    benchmark::RegisterBenchmark("Forward Kinematics 10pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 10 );
-
-
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
-
-
-//    benchmark::RegisterBenchmark("Forward Kinematics 15pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 15 );
-
-
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
-
-
-    CROSP::CosseratRod rod;
-    benchmark::RegisterBenchmark("Forward Kinematics 16pt", [&](benchmark::State &t_state){
+        CROSP::CosseratRod rod(10);
 
         while(t_state.KeepRunning())
             rod.forwardKinematics();
     })->Repetitions(10);
 
 
-//    benchmark::RegisterBenchmark("Forward Kinematics 20pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 20 );
+    benchmark::RegisterBenchmark("Forward Kinematics 15", [](benchmark::State &t_state){
 
+        CROSP::CosseratRod rod(15);
 
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
+        while(t_state.KeepRunning())
+            rod.forwardKinematics();
+    })->Repetitions(10);
 
-//    benchmark::RegisterBenchmark("Forward Kinematics 25pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 25 );
+    benchmark::RegisterBenchmark("Forward Kinematics 20", [](benchmark::State &t_state){
 
+        CROSP::CosseratRod rod(20);
 
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
+        while(t_state.KeepRunning())
+            rod.forwardKinematics();
+    })->Repetitions(10);
 
-//    benchmark::RegisterBenchmark("Forward Kinematics 30pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 30 );
+    benchmark::RegisterBenchmark("Forward Kinematics 25", [](benchmark::State &t_state){
 
+        CROSP::CosseratRod rod(25);
 
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
+        while(t_state.KeepRunning())
+            rod.forwardKinematics();
+    })->Repetitions(10);
 
-//    benchmark::RegisterBenchmark("Forward Kinematics 35pt", [](benchmark::State &t_state){
-//        CROSP::CosseratRod rod( 35 );
+    benchmark::RegisterBenchmark("Forward Kinematics 30", [](benchmark::State &t_state){
 
+        CROSP::CosseratRod rod(30);
 
-//        while(t_state.KeepRunning())
-//            rod.forwardKinematics();
-//    })->Repetitions(10);
+        while(t_state.KeepRunning())
+            rod.forwardKinematics();
+    })->Repetitions(10);
+
+    benchmark::RegisterBenchmark("Forward Kinematics 35", [](benchmark::State &t_state){
+
+        CROSP::CosseratRod rod(35);
+
+        while(t_state.KeepRunning())
+            rod.forwardKinematics();
+    })->Repetitions(10);
 
 
     benchmark::Initialize(&argc, argv);
