bf3772f4d8668004654d755f87dbb81ac97fea95
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-28 16:37:32 +0200

    correct initialization

--- include/tidm_integrators.hpp
+++ include/tidm_integrators.hpp
@@ -26,8 +26,8 @@ namespace CROSP {
 
 
 struct DeltaRotation : public OSNI::ODEAb {
-    DeltaRotation(const std::shared_ptr<const std::vector<Eigen::Vector3d>> t_K_stack,
-                  const std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Delta_K_stack,
+    DeltaRotation(std::shared_ptr<const std::vector<Eigen::Vector3d>> t_K_stack,
+                  std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Delta_K_stack,
                   const unsigned int t_number_of_Chebyshev_points) :    OSNI::ODEAb(4, ::Chebyshev::INTEGRATION_DIRECTION::FORWARD,
                                                                                     t_number_of_Chebyshev_points),
                                                                         m_K_stack(t_K_stack),
@@ -51,10 +51,10 @@ struct DeltaRotation : public OSNI::ODEAb {
 
 struct DeltaPosition : public OSNI::ODEAb {
 
-    DeltaPosition(const std::shared_ptr<const std::vector<Eigen::Vector3d>> t_K_stack,
-                  const std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Lambda_stack,
-                  const std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Delta_Lambda_stack,
-                  const std::shared_ptr<const DeltaRotation> t_delta_rotation,
+    DeltaPosition(std::shared_ptr<const std::vector<Eigen::Vector3d>> t_K_stack,
+                  std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Lambda_stack,
+                  std::shared_ptr<const std::vector<Eigen::Vector3d>> t_Delta_Lambda_stack,
+                  std::shared_ptr<const DeltaRotation> t_delta_rotation,
                   const unsigned int t_number_of_Chebyshev_points) :    OSNI::ODEAb(4, ::Chebyshev::INTEGRATION_DIRECTION::FORWARD,
                                                                                     t_number_of_Chebyshev_points),
                                                                         m_K_stack(t_K_stack),
@@ -84,14 +84,18 @@ struct DeltaPosition : public OSNI::ODEAb {
 
 struct TIDMIntegrators{
 
-    TIDMIntegrators(const std::shared_ptr<const StrainParameterisation> &t_strain_parameterisation,
-                    const std::shared_ptr<const IDMIntegrators> &t_idm_integrators) :   m_strain_parameterisation(t_strain_parameterisation),
-                                                                                        m_idm_integrators(t_idm_integrators)
+    TIDMIntegrators(std::shared_ptr<const StrainParameterisation> t_strain_parameterisation,
+                    std::shared_ptr<const StrainParameterisationPerturbation> t_strain_parameterisation_perturbation,
+                    std::shared_ptr<const IDMIntegrators> t_idm_integrators) :  m_strain_parameterisation(t_strain_parameterisation),
+                                                                                m_strain_parameterisation_perturbation(t_strain_parameterisation_perturbation),
+                                                                                m_idm_integrators(t_idm_integrators)
     {}
 
-    TIDMIntegrators(const std::shared_ptr<const StrainParameterisation> t_strain_parameterisation,
-                    const std::shared_ptr<const IDMIntegrators> &t_idm_integrators,
+    TIDMIntegrators(std::shared_ptr<const StrainParameterisation> t_strain_parameterisation,
+                    std::shared_ptr<const StrainParameterisationPerturbation> t_strain_parameterisation_perturbation,
+                    std::shared_ptr<const IDMIntegrators> t_idm_integrators,
                     const unsigned int t_number_of_Chebyshev_points) :  m_strain_parameterisation(t_strain_parameterisation),
+                                                                        m_strain_parameterisation_perturbation(t_strain_parameterisation_perturbation),
                                                                         m_idm_integrators(t_idm_integrators),
                                                                         m_number_of_Chebyshev_points(t_number_of_Chebyshev_points)
     {}
@@ -99,19 +103,19 @@ struct TIDMIntegrators{
 
 
     const std::shared_ptr<const StrainParameterisation> m_strain_parameterisation;
-    const std::shared_ptr<const StrainParameterisation> m_Delta_strain_parameterisation;
+    const std::shared_ptr<const StrainParameterisationPerturbation> m_strain_parameterisation_perturbation;
     const std::shared_ptr<const IDMIntegrators> m_idm_integrators;
 
     const unsigned int m_number_of_Chebyshev_points { 17 };
 
 
     std::shared_ptr<DeltaRotation> m_delta_rotation { std::make_shared<DeltaRotation>( m_strain_parameterisation->m_K_stack,
-                                                                                       m_Delta_strain_parameterisation->m_K_stack,
+                                                                                       m_strain_parameterisation_perturbation->m_Delta_K_stack,
                                                                                        m_number_of_Chebyshev_points) };
 
     std::shared_ptr<DeltaPosition> m_delta_position { std::make_shared<DeltaPosition>( m_strain_parameterisation->m_K_stack,
                                                                                        m_strain_parameterisation->m_Lambda_stack,
-                                                                                       m_Delta_strain_parameterisation->m_Lambda_stack,
+                                                                                       m_strain_parameterisation_perturbation->m_Delta_Lambda_stack,
                                                                                        m_delta_rotation,
                                                                                        m_number_of_Chebyshev_points) };
 
