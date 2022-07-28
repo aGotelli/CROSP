1feb9630f2268cd334f8c8cbe6f45e8f8558bdc1
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-28 10:40:35 +0200

    moving functions and code to the strain_parameterisation file

--- src/cosserat_rod.cpp
+++ src/cosserat_rod.cpp
@@ -24,26 +24,29 @@ void CosseratRod::updateParameterisation(const Eigen::VectorXd &t_qe,
                                          const Eigen::VectorXd &t_dot_qe,
                                          const Eigen::VectorXd &t_ddot_qe)
 {
-    Eigen::MatrixXd Phi(m_na, m_ne*m_na);
+//    Eigen::MatrixXd Phi(m_na, m_ne*m_na);
 
-    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
-        const double point = m_Chebyshev_points[i];
+//    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
+//        const double point = m_Chebyshev_points[i];
 
-        Phi = getPhi( point );
+//        Phi = getPhi( point );
 
-        m_K_stack->at(i) = Phi*t_qe;
+//        m_K_stack->at(i) = Phi*t_qe;
 
-        std::cout << "The point : " << i << " at : " << point << " K : " << m_K_stack->at(i).transpose() << std::endl;
-        m_dot_K_stack->at(i) = Phi*t_dot_qe;
-        m_ddot_K_stack->at(i) = Phi*t_ddot_qe;
+//        std::cout << "The point : " << i << " at : " << point << " K : " << m_K_stack->at(i).transpose() << std::endl;
+//        m_dot_K_stack->at(i) = Phi*t_dot_qe;
+//        m_ddot_K_stack->at(i) = Phi*t_ddot_qe;
 
-    }
+//    }
 
-    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
-        m_Lambda_stack->at(i) = Eigen::Vector3d(1, 0, 0);
-        m_dot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
-        m_ddot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
-    }
+//    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
+//        m_Lambda_stack->at(i) = Eigen::Vector3d(1, 0, 0);
+//        m_dot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
+//        m_ddot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
+//    }
+
+
+    m_strain_parameterisation.update(t_qe, t_dot_qe, t_ddot_qe, m_Phi_stack);
 }
 
 void CosseratRod::forwardKinematics(const Eigen::Vector4d &t_initial_quaternion,
@@ -65,12 +68,12 @@ void CosseratRod::forwardKinematics(const Eigen::Vector4d &t_initial_quaternion,
 
     m_linear_acceleration_integrator->integrate(t_initial_linear_acceleration);
 
-    std::cout << "Quaternions : \n" << m_quaternion_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Positions : \n" << m_position_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Linear velocities : \n" << m_angular_velocity_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Angular velocities : \n" << m_linear_velocity_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Linear accelerations : \n" << m_angular_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Angular accelerations : \n" << m_linear_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Quaternions : \n" << m_quaternion_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Positions : \n" << m_position_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Linear velocities : \n" << m_angular_velocity_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Angular velocities : \n" << m_linear_velocity_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Linear accelerations : \n" << m_angular_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Angular accelerations : \n" << m_linear_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
 
 }
 
@@ -107,30 +110,10 @@ void CosseratRod::backwardDynamics(const Eigen::Vector3d &t_force_at_tip,
     m_internal_forces_integrator->integrate(force_at_tip_local_coord);
     m_internal_couples_integrator->integrate(couple_at_tip_local_coord);
 
-    std::cout << "Internal forces : \n" << m_internal_forces_integrator->getStack() << "\n\n\n" << std::endl;
-    std::cout << "Internal couples : \n" << m_internal_couples_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Internal forces : \n" << m_internal_forces_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Internal couples : \n" << m_internal_couples_integrator->getStack() << "\n\n\n" << std::endl;
 }
 
-
-Eigen::MatrixXd CosseratRod::getPhi(const double& t_X,
-                                    const double& t_begin,
-                                    const double& t_end) const
-{
-    //  The coordinate must be transposed into the Chebyshev domain [-1, 1];
-    double x = ( 2 * t_X - ( t_end + t_begin) ) / ( t_end - t_begin );
-
-    //  Compute the values of the polynomial for every element of the strain field
-    Eigen::VectorXd Phi_i(m_ne, 1);
-    for(unsigned int i=0; i<m_ne; i++)
-        Phi_i[i] = m_polynomial_base(i, x);
-
-
-    //  Define the matrix of bases
-    Eigen::MatrixXd Phi = Eigen::KroneckerProduct(Eigen::MatrixXd::Identity(m_na, m_na), Phi_i.transpose());
-
-
-    return Phi;
-}
 
 
 
