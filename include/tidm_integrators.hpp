c1814aa8d23ab59ecc364385f73d39a0695509ac
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-28 14:06:21 +0200

    added header file for TIDM integrators

---
+++ include/tidm_integrators.hpp
@@ -0,0 +1,16 @@
+/**
+ * \file tidm_integrators.hpp
+ * \author Andrea Gotelli (Andrea.Gotelli@ls2n.fr)
+ * \brief This files contains the integrators for the Cosserat rod TIDM
+ * \date 28-07-2022
+ *
+ * \copyright Copyright (c) 2022 Andrea Gotelli
+ *
+ *
+ */
+
+
+#ifndef TIDM_INTEGRATORS_HPP
+#define TIDM_INTEGRATORS_HPP
+
+#endif // TIDM_INTEGRATORS_HPP
