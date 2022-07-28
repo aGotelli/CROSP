6d201e39f997ef0e49ccf20a18da5b8ef1d6bf32
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-28 12:08:18 +0200

    simplified the body of the object
    simplified the body of the object using the other objects StrainParameterisation and IDMIntegrators

--- include/cosserat_rod.hpp
+++ include/cosserat_rod.hpp
@@ -93,85 +93,11 @@ public:
 private:
 #endif
 
-
-    const unsigned int m_ne { 3 };
-    const unsigned int m_na { 3 };
-
-
-    const unsigned int m_number_of_chebyshev_points { 17 };
-
-    const std::vector<double> m_Chebyshev_points { ::Chebyshev::ComputeChebyshevPoints(m_number_of_chebyshev_points) };
-
-    const std::function<double(const unsigned int, const double&)> m_polynomial_base { [](const unsigned int t_point, const double& t_x) {return boost::math::legendre_p(t_point, t_x);} };
-
     MaterialProperties m_material_properties;
 
-    StrainParameterisation m_strain_parameterisation { StrainParameterisation(generatePhiStack(m_ne, m_na, m_Chebyshev_points, m_polynomial_base)) };
+    std::shared_ptr<StrainParameterisation> m_strain_parameterisation { std::make_shared<StrainParameterisation>() };
 
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_K_stack { std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_Lambda_stack { std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_dot_K_stack { std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_dot_Lambda_stack{ std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_ddot_K_stack { std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-    std::shared_ptr<std::vector<Eigen::Vector3d>> m_ddot_Lambda_stack { std::make_shared<std::vector<Eigen::Vector3d>>(m_number_of_chebyshev_points) };
-
-
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_quaternion_integrator { std::make_shared<QuaternionIntegrator>(m_K_stack,
-                                                                                                               m_number_of_chebyshev_points ) };
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_position_integrator { std::make_shared<PositionIntegrator>(m_quaternion_integrator,
-                                                                                                           m_Lambda_stack,
-                                                                                                           m_number_of_chebyshev_points ) };
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_angular_velocity_integrator { std::make_shared<AngularVelocityIntegrator>(m_K_stack,
-                                                                                                                          m_dot_K_stack,
-                                                                                                                          m_number_of_chebyshev_points ) };
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_linear_velocity_integrator { std::make_shared<LinearVelocityIntegrator>(m_K_stack,
-                                                                                                                        m_Lambda_stack,
-                                                                                                                        m_dot_Lambda_stack,
-                                                                                                                        m_angular_velocity_integrator,
-                                                                                                                        m_number_of_chebyshev_points ) };
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_angular_acceleration_integrator { std::make_shared<AngularAccelerationIntegrator>(m_K_stack,
-                                                                                                                                  m_dot_K_stack,
-                                                                                                                                  m_ddot_K_stack,
-                                                                                                                                  m_angular_velocity_integrator,
-                                                                                                                                  m_number_of_chebyshev_points ) };
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_linear_acceleration_integrator { std::make_shared<LinearAccelerationIntegrator>(m_K_stack,
-                                                                                                                                m_dot_K_stack,
-                                                                                                                                m_Lambda_stack,
-                                                                                                                                m_dot_Lambda_stack,
-                                                                                                                                m_ddot_Lambda_stack,
-                                                                                                                                m_angular_velocity_integrator,
-                                                                                                                                m_linear_velocity_integrator,
-                                                                                                                                m_angular_acceleration_integrator,
-                                                                                                                                m_number_of_chebyshev_points ) };
-
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_internal_forces_integrator { std::make_shared<InternalForcesIntegrator>(m_material_properties.m_M.block<3,3>(3, 3),
-                                                                                                                        m_K_stack, m_angular_velocity_integrator,
-                                                                                                                        m_linear_velocity_integrator,
-                                                                                                                        m_linear_acceleration_integrator,
-                                                                                                                        m_quaternion_integrator,
-                                                                                                                        m_position_integrator,
-                                                                                                                        m_number_of_chebyshev_points )};
-
-    std::shared_ptr<OSNI::ODESolverInterface> m_internal_couples_integrator { std::make_shared<InternalCouplesIntegrator>(m_material_properties.m_M.block<3,3>(0, 0),
-                                                                                                                          m_material_properties.m_M.block<3,3>(3, 3),
-                                                                                                                          m_K_stack,
-                                                                                                                          m_Lambda_stack,
-                                                                                                                          m_angular_velocity_integrator,
-                                                                                                                          m_linear_velocity_integrator,
-                                                                                                                          m_angular_acceleration_integrator,
-                                                                                                                          m_quaternion_integrator,
-                                                                                                                          m_position_integrator,
-                                                                                                                          m_internal_forces_integrator,
-                                                                                                                          m_number_of_chebyshev_points )};
+    std::shared_ptr<IDMIntegrators> m_idm_integrators { std::make_shared<IDMIntegrators>(m_strain_parameterisation, m_material_properties.m_M.block<3,3>(0,0), m_material_properties.m_M.block<3,3>(3,3))};
 
 };
 
--- src/cosserat_rod.cpp
+++ src/cosserat_rod.cpp
@@ -17,36 +17,36 @@
 namespace CROSP {
 
 
-CosseratRod::CosseratRod(const unsigned int t_number_of_chebyshev_points) : m_number_of_chebyshev_points(t_number_of_chebyshev_points)
+CosseratRod::CosseratRod(const unsigned int t_number_of_chebyshev_points) : m_strain_parameterisation( std::make_shared<StrainParameterisation>(t_number_of_chebyshev_points) )
 {}
 
 void CosseratRod::updateParameterisation(const Eigen::VectorXd &t_qe,
                                          const Eigen::VectorXd &t_dot_qe,
                                          const Eigen::VectorXd &t_ddot_qe)
 {
-    Eigen::MatrixXd Phi(m_na, m_ne*m_na);
+//    Eigen::MatrixXd Phi(m_na, m_ne*m_na);
 
-    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
-        const double point = m_Chebyshev_points[i];
+//    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
+//        const double point = m_Chebyshev_points[i];
 
-        Phi = getPhi( point );
+//        Phi = getPhi( point );
 
-        m_K_stack->at(i) = Phi*t_qe;
+//        m_K_stack->at(i) = Phi*t_qe;
 
-        std::cout << "The point : " << i << " at : " << point << " K : " << m_K_stack->at(i).transpose() << std::endl;
-        m_dot_K_stack->at(i) = Phi*t_dot_qe;
-        m_ddot_K_stack->at(i) = Phi*t_ddot_qe;
+//        std::cout << "The point : " << i << " at : " << point << " K : " << m_K_stack->at(i).transpose() << std::endl;
+//        m_dot_K_stack->at(i) = Phi*t_dot_qe;
+//        m_ddot_K_stack->at(i) = Phi*t_ddot_qe;
 
-    }
+//    }
 
-    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
-        m_Lambda_stack->at(i) = Eigen::Vector3d(1, 0, 0);
-        m_dot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
-        m_ddot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
-    }
+//    for(unsigned int i=0; i<m_number_of_chebyshev_points; i++){
+//        m_Lambda_stack->at(i) = Eigen::Vector3d(1, 0, 0);
+//        m_dot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
+//        m_ddot_Lambda_stack->at(i) = Eigen::Vector3d::Zero();
+//    }
 
 
-    //m_strain_parameterisation.update(t_qe, t_dot_qe, t_ddot_qe, m_Phi_stack);
+    m_strain_parameterisation->update(t_qe, t_dot_qe, t_ddot_qe);
 }
 
 void CosseratRod::forwardKinematics(const Eigen::Vector4d &t_initial_quaternion,
@@ -56,24 +56,24 @@ void CosseratRod::forwardKinematics(const Eigen::Vector4d &t_initial_quaternion,
                                     const Eigen::Vector3d &t_initial_angular_acceleration,
                                     const Eigen::Vector3d &t_initial_linear_acceleration)
 {
-    m_quaternion_integrator->integrate(t_initial_quaternion);
+    m_idm_integrators->m_quaternion->integrate(t_initial_quaternion);
 
-    m_position_integrator->integrate(t_initial_position);
+    m_idm_integrators->m_position->integrate(t_initial_position);
 
-    m_angular_velocity_integrator->integrate(t_initial_angular_velocity);
+    m_idm_integrators->m_angular_velocity->integrate(t_initial_angular_velocity);
 
-    m_linear_velocity_integrator->integrate(t_initial_linear_velocity);
+    m_idm_integrators->m_linear_velocity->integrate(t_initial_linear_velocity);
 
-    m_angular_acceleration_integrator->integrate(t_initial_angular_acceleration);
+    m_idm_integrators->m_angular_acceleration->integrate(t_initial_angular_acceleration);
 
-    m_linear_acceleration_integrator->integrate(t_initial_linear_acceleration);
+    m_idm_integrators->m_linear_acceleration->integrate(t_initial_linear_acceleration);
 
-//    std::cout << "Quaternions : \n" << m_quaternion_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Positions : \n" << m_position_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Linear velocities : \n" << m_angular_velocity_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Angular velocities : \n" << m_linear_velocity_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Linear accelerations : \n" << m_angular_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Angular accelerations : \n" << m_linear_acceleration_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Quaternions : \n" << m_idm_integrators->m_quaternion->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Positions : \n" << m_idm_integrators->m_position->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Linear velocities : \n" << m_idm_integrators->m_angular_velocity->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Angular velocities : \n" << m_idm_integrators->m_linear_velocity->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Linear accelerations : \n" << m_idm_integrators->m_angular_acceleration->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Angular accelerations : \n" << m_idm_integrators->m_linear_acceleration->getStack() << "\n\n\n" << std::endl;
 
 }
 
@@ -82,15 +82,15 @@ void CosseratRod::forwardKinematics(const Eigen::Vector4d &t_initial_quaternion,
 {
     ::LieAlgebra::Kinematics rod_tip_kinematics;
 
-    rod_tip_kinematics.m_pose = ::LieAlgebra::SE3Pose( m_quaternion_integrator->getStateAtPoint(0),
-                                                       m_position_integrator->getStateAtPoint(0) );
+    rod_tip_kinematics.m_pose = ::LieAlgebra::SE3Pose( m_idm_integrators->m_quaternion->getStateAtPoint(0),
+                                                       m_idm_integrators->m_position->getStateAtPoint(0) );
 
 
-    rod_tip_kinematics.m_twist = ::LieAlgebra::Screw( m_angular_velocity_integrator->getStateAtPoint(0),
-                                                      m_linear_velocity_integrator->getStateAtPoint(0) );
+    rod_tip_kinematics.m_twist = ::LieAlgebra::Screw( m_idm_integrators->m_angular_velocity->getStateAtPoint(0),
+                                                      m_idm_integrators->m_linear_velocity->getStateAtPoint(0) );
 
-    rod_tip_kinematics.m_accelerations = ::LieAlgebra::Screw( m_angular_acceleration_integrator->getStateAtPoint(0),
-                                                              m_linear_acceleration_integrator->getStateAtPoint(0) );
+    rod_tip_kinematics.m_accelerations = ::LieAlgebra::Screw( m_idm_integrators->m_angular_acceleration->getStateAtPoint(0),
+                                                              m_idm_integrators->m_linear_acceleration->getStateAtPoint(0) );
     return rod_tip_kinematics;
 }
 
@@ -107,32 +107,32 @@ void CosseratRod::backwardDynamics(const Eigen::Vector3d &t_force_at_tip,
 
     Eigen::Vector3d couple_at_tip_local_coord = tip_pose.getRotationMatrix().transpose()*t_couple_at_tip;
 
-    m_internal_forces_integrator->integrate(force_at_tip_local_coord);
-    m_internal_couples_integrator->integrate(couple_at_tip_local_coord);
+    m_idm_integrators->m_internal_forces->integrate(force_at_tip_local_coord);
+    m_idm_integrators->m_internal_couples->integrate(couple_at_tip_local_coord);
 
-//    std::cout << "Internal forces : \n" << m_internal_forces_integrator->getStack() << "\n\n\n" << std::endl;
-//    std::cout << "Internal couples : \n" << m_internal_couples_integrator->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Internal forces : \n" << m_idm_integrators->m_internal_forces->getStack() << "\n\n\n" << std::endl;
+//    std::cout << "Internal couples : \n" << m_idm_integrators->m_internal_couples->getStack() << "\n\n\n" << std::endl;
 }
 
-Eigen::MatrixXd CosseratRod::getPhi(const double& t_X,
-                                    const double& t_begin,
-                                    const double& t_end)const
-{
-    //  The coordinate must be transposed into the Chebyshev domain [-1, 1];
-    double x = ( 2 * t_X - ( t_end + t_begin) ) / ( t_end - t_begin );
+//Eigen::MatrixXd CosseratRod::getPhi(const double& t_X,
+//                                    const double& t_begin,
+//                                    const double& t_end)const
+//{
+//    //  The coordinate must be transposed into the Chebyshev domain [-1, 1];
+//    double x = ( 2 * t_X - ( t_end + t_begin) ) / ( t_end - t_begin );
 
-    //  Compute the values of the polynomial for every element of the strain field
-    Eigen::VectorXd Phi_i(m_ne, 1);
-    for(unsigned int i=0; i<m_ne; i++)
-        Phi_i[i] = m_polynomial_base(i, x);
-
-
-    //  Define the matrix of bases
-    Eigen::MatrixXd Phi = Eigen::KroneckerProduct(Eigen::MatrixXd::Identity(m_na, m_na), Phi_i.transpose());
+//    //  Compute the values of the polynomial for every element of the strain field
+//    Eigen::VectorXd Phi_i(m_ne, 1);
+//    for(unsigned int i=0; i<m_ne; i++)
+//        Phi_i[i] = m_polynomial_base(i, x);
 
 
-    return Phi;
-}
+//    //  Define the matrix of bases
+//    Eigen::MatrixXd Phi = Eigen::KroneckerProduct(Eigen::MatrixXd::Identity(m_na, m_na), Phi_i.transpose());
+
+
+//    return Phi;
+//}
 
 
 }   //  namespace CROSP
