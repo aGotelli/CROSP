0227856dac0327cc00fdf8b9ecc47691977903d8
Andrea Gotelli <gotelliandrea@gmail.com>
2022-11-25 10:19:25 +0100

    added functionality to have the rod automatically initialized
    I added a functionality to have the rod to go full straight on its x axis by default

--- src/include/CROSP/CROSP/cosserat_rod.hpp
+++ src/include/CROSP/CROSP/cosserat_rod.hpp
@@ -392,8 +392,18 @@ private:
     std::shared_ptr<rod_properties::RodProperties> m_rod_properties { std::make_shared<rod_properties::RodProperties>(m_polynomial_representation) };
 
     //  All the strain releted variables
-    std::shared_ptr<strain_parameterisation::StrainParameterisation> m_strain_parameterisation { std::make_shared<strain_parameterisation::StrainParameterisation>(m_polynomial_representation,
-                                                                                                                                                                   m_number_of_Chebyshev_points) };
+    std::shared_ptr<strain_parameterisation::StrainParameterisation> m_strain_parameterisation { [this](){
+
+        auto strain_parameterisation = std::make_shared<strain_parameterisation::StrainParameterisation>(m_polynomial_representation,
+                                                                                                         m_number_of_Chebyshev_points);
+
+        const unsigned int ne = m_polynomial_representation.getCoordinatesDimension();
+        strain_parameterisation->updateStacks(Eigen::VectorXd::Zero(ne),
+                                              Eigen::VectorXd::Zero(ne),
+                                              Eigen::VectorXd::Zero(ne));
+
+        return strain_parameterisation;
+    }() };
 
     //  Variables related the perturbation of the strain parameterisation
     std::shared_ptr<strain_parameterisation::StrainParameterisation> m_strain_parameterisation_Delta { std::make_shared<strain_parameterisation::StrainParameterisation>(m_polynomial_representation,
