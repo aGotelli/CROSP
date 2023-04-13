8697fc3cf41f9237c774866c6625ef6dbde1cbe2
Andrea Gotelli <gotelliandrea@gmail.com>
2023-04-13 12:42:02 +0200

    simplied the class

--- src/include/CROSP/CROSP/internally_actuated_cosserat_rod.hpp
+++ src/include/CROSP/CROSP/internally_actuated_cosserat_rod.hpp
@@ -3,79 +3,9 @@
 
 #include "cosserat_rod.hpp"
 
+#include "ATORS/tendon_driven_actuation/tendon_driven_actuation.hpp"
+
 namespace CROSP {
-
-
-struct Tendon{
-
-    Tendon(const Eigen::Vector3d &t_D,
-           const Eigen::Vector3d &t_D_prime=Eigen::Vector3d::Zero())
-        : m_D(t_D),
-          m_D_prime(t_D_prime)
-    {}
-
-
-    Eigen::Vector3d m_D;
-    Eigen::Vector3d m_D_prime;
-
-    ::LieAlgebra::Vector6d getStrain(const double &t_tau,
-                                     const Eigen::Vector3d &t_K,
-                                     const Eigen::Vector3d &t_Gamma)
-    {
-        const auto K_skew = ::LieAlgebra::skew(t_K);
-        const auto D_skew = ::LieAlgebra::skew(m_D);
-
-        const Eigen::Vector3d T = t_Gamma + K_skew*m_D + m_D_prime;
-
-        ::LieAlgebra::Vector6d strain_map;
-        strain_map <<  D_skew*T,
-                           T   ;
-
-
-        const double norm = T.norm();
-
-        const ::LieAlgebra::Vector6d strain_from_cable = strain_map*t_tau / norm;
-
-        return strain_from_cable;
-    }
-
-
-};
-
-struct Actuator {
-    std::function<void(const double&)> updateTorque;
-
-    double m_tau;
-};
-
-typedef std::pair<Actuator, Tendon> ActuatedTendon;
-
-
-struct InternalActuation{
-
-
-    void addTendon(const Tendon &t_tendon,
-                   const Actuator &t_actuator);
-
-
-    std::vector<ActuatedTendon> m_actuated_tendons;
-
-};
-
-struct GeneralisedInternalActuationIntegrator : public OSNI::ODEb {
-
-    GeneralisedInternalActuationIntegrator(std::shared_ptr<const strain_parameterisation::StrainParameterisation> t_strain_parameterisation,
-                                           InternalActuation t_internal_actuation);
-
-    virtual Eigen::VectorXd computerParametersVectorAtPoint(const unsigned int t_point) final;
-
-    std::shared_ptr<const strain_parameterisation::StrainParameterisation> m_strain_parameterisation;
-
-    InternalActuation m_internal_actuation;
-
-    Eigen::VectorXd m_tau;
-
-};
 
 
 
@@ -87,40 +17,53 @@ public:
 
     /*!
      * \brief InternallyActuatedCosseratRod initialised the rod by giving its properties anly
+     * \param t_actuated_tendons is the set of actuated tendons acting on the rod
+     *
+     * In this constructo the strain parameterisation is initialised by its defaul value
+     */
+    InternallyActuatedCosseratRod(const ::ATORS::tendon_driven_actuation::ActuatedTendons t_actuated_tendons);
+
+
+
+    /*!
+     * \brief InternallyActuatedCosseratRod initialised the rod by giving its properties anly
      * \param t_rod_properties is the sef of desiderd properties as a ::CROSP::rod_properties::RodProperties object
+     * \param t_actuated_tendons is the set of actuated tendons acting on the rod
      *
      * In this constructo the strain parameterisation is initialised by its defaul value
      */
     InternallyActuatedCosseratRod(const std::shared_ptr<const rod_properties::RodProperties> t_rod_properties,
-                                  const InternalActuation t_actuation);
+                                  const ::ATORS::tendon_driven_actuation::ActuatedTendons t_actuated_tendons);
 
 
     /*!
      * \brief InternallyActuatedCosseratRod initialised the rod by giving its strain parameterisation
      * \param t_strain_parameterisation is the sef of desiderd parameterisation of the rod strain field as a ::CROSP::strain_parameterisation::StrainParameterisation object
+     * \param t_actuated_tendons is the set of actuated tendons acting on the rod
      *
      * In this constructo the rod properties is initialised by its defaul value
      */
     InternallyActuatedCosseratRod(const std::shared_ptr<strain_parameterisation::StrainParameterisation> t_strain_parameterisation,
-                                  const InternalActuation t_actuation);
+                                  const ::ATORS::tendon_driven_actuation::ActuatedTendons t_actuated_tendons);
 
 
     /*!
      * \brief InternallyActuatedCosseratRod this constructor requires both argument to initialise a custom Cosserat rod
      * \param t_strain_parameterisation is the sef of desiderd parameterisation of the rod strain field as a ::CROSP::strain_parameterisation::StrainParameterisation object
      * \param t_rod_properties is the sef of desiderd properties as a ::CROSP::rod_properties::RodProperties object
+     * \param t_actuated_tendons is the set of actuated tendons acting on the rod
      */
     InternallyActuatedCosseratRod(const std::shared_ptr<strain_parameterisation::StrainParameterisation> t_strain_parameterisation,
                                   const std::shared_ptr<const rod_properties::RodProperties> t_rod_properties,
-                                  const InternalActuation t_actuation);
+                                  const ::ATORS::tendon_driven_actuation::ActuatedTendons t_actuated_tendons);
 
 
     virtual void updateInternalActuation(const double &t_current_time) override;
 
-    virtual Eigen::VectorXd getQad() override;
+    virtual Eigen::VectorXd getQad() const override;
 
 
-    GeneralisedInternalActuationIntegrator m_internal_actuation_integrator;
+    ::ATORS::tendon_driven_actuation::TendonDrivenActuation m_tendon_driven_actuator;
 
 
 };
