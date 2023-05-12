0cb1329b881730f9ed215548bc2e9394a98d0de9
Andrea Gotelli <gotelliandrea@gmail.com>
2023-05-12 15:32:03 -0400

    ok cleaned from old files

--- src/include/CROSP/numerical_integrators/spectral/parameterisation_stack.hpp
+++ src/include/CROSP/numerical_integrators/spectral/parameterisation_stack.hpp
@@ -16,57 +16,20 @@ struct ParameterisationStack {
 
     ParameterisationStack(const strain_parameterisation::StrainParameterisation t_strain_parameterisation,
                           const polynomial_representation::PolynomialRepresentation t_polynomial_representation,
-                          const unsigned int t_number_of_Chebyshev_points)
-        : m_number_of_Chebyshev_points(t_number_of_Chebyshev_points),
-          m_strain_parameterisation(t_strain_parameterisation),
-          m_polynomial_representation(t_polynomial_representation)
-    {
-        //  Default initialisation with nominal strain
-        Eigen::VectorXd zeros = Eigen::VectorXd::Zero(m_polynomial_representation.getCoordinatesDimension());
-        updateStacks(zeros, zeros, zeros);
-    }
+                          const unsigned int t_number_of_Chebyshev_points);
 
 
 
 
+    /*!
+     * \brief updateStacks updates the stacks of strain and its first and second derivative
+     * \param t_qe the generalised elastic coordinates
+     * \param t_dot_qe the first derivative of the generalised elastic coordinates
+     * \param t_ddot_qe the second derivative of the generalised elastic coordinates
+     */
     void updateStacks(const Eigen::VectorXd &t_qe,
-                                             const Eigen::VectorXd &t_dot_qe,
-                                             const Eigen::VectorXd &t_ddot_qe)
-    {
-
-        ::LieAlgebra::Vector6d xi;
-        ::LieAlgebra::Vector6d dot_xi;
-        ::LieAlgebra::Vector6d ddot_xi;
-
-
-        for(unsigned int i=0; i<m_number_of_Chebyshev_points; i++){
-
-            xi      = m_map_to_strain_stack[i]*t_qe + m_strain_parameterisation.m_constant_strain;
-            dot_xi  = m_map_to_strain_stack[i]*t_dot_qe;
-            ddot_xi = m_map_to_strain_stack[i]*t_ddot_qe;
-
-            m_K_stack->at(i)      = xi.block<3,1>(0,0);
-            m_dot_K_stack->at(i)  = dot_xi.block<3,1>(0,0);
-            m_ddot_K_stack->at(i) = ddot_xi.block<3,1>(0,0);
-
-            m_Gamma_stack->at(i)      = xi.block<3,1>(3,0);
-            m_dot_Gamma_stack->at(i)  = dot_xi.block<3,1>(3,0);
-            m_ddot_Gamma_stack->at(i) = ddot_xi.block<3,1>(3,0);
-
-        }
-
-    }
-
-
-//    /*!
-//     * \brief updateStacks updates the stacks of strain and its first and second derivative
-//     * \param t_qe the generalised elastic coordinates
-//     * \param t_dot_qe the first derivative of the generalised elastic coordinates
-//     * \param t_ddot_qe the second derivative of the generalised elastic coordinates
-//     */
-//    void updateStacks(const Eigen::VectorXd &t_qe,
-//                      const Eigen::VectorXd &t_dot_qe,
-//                      const Eigen::VectorXd &t_ddot_qe);
+                      const Eigen::VectorXd &t_dot_qe,
+                      const Eigen::VectorXd &t_ddot_qe);
 
 
 
