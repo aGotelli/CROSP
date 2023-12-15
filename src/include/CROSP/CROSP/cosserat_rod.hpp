aab238c22d7a963f6f172b1b069e4f3e907fc59e
Andrea Gotelli <gotelliandrea@gmail.com>
2023-12-15 18:45:27 +0100

    downscale to rule of 3

--- src/include/CROSP/CROSP/cosserat_rod.hpp
+++ src/include/CROSP/CROSP/cosserat_rod.hpp
@@ -116,12 +116,12 @@ public:
     {}
 
 
-    CosseratRod(CosseratRod &&t_other)
-        : m_polynomial_representation(t_other.m_polynomial_represenation),
-          m_rod_properties( std::move(t_other.m_rod_properties) ),
-          m_constrained_strain(t_other.m_constrained_strain),
-          m_number_of_Chebyshev_points(t_other.m_number_of_Chebyshev_points)
-    {}
+//    CosseratRod(CosseratRod &&t_other)
+//        : m_polynomial_representation(t_other.m_polynomial_represenation),
+//          m_rod_properties( std::move(t_other.m_rod_properties) ),
+//          m_constrained_strain(t_other.m_constrained_strain),
+//          m_number_of_Chebyshev_points(t_other.m_number_of_Chebyshev_points)
+//    {}
 
 
 
@@ -136,15 +136,15 @@ public:
                            t_other.m_constrained_strain);
     }
 
-    CosseratRod operator=(CosseratRod &&t_other)
-    {
+//    CosseratRod operator=(CosseratRod &&t_other)
+//    {
 
-        return CosseratRod(t_other.m_polynomial_representation,
-                           t_other.m_number_of_Chebyshev_points,
-                           std::move(t_other.m_rod_properties->m_rod_dimensions),
-                           t_other.m_rod_properties->m_material_properties,
-                           t_other.m_constrained_strain);
-    }
+//        return CosseratRod(t_other.m_polynomial_representation,
+//                           t_other.m_number_of_Chebyshev_points,
+//                           std::move(t_other.m_rod_properties->m_rod_dimensions),
+//                           t_other.m_rod_properties->m_material_properties,
+//                           t_other.m_constrained_strain);
+//    }
 
 
     CosseratRod makeCopy() const
